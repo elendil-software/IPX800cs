a488a46752e23d2f2cdc3c19495952f40e579d5b
Julien Tschäppät <git@elendil.software>
2017-06-24 21:00:36 +0200

    New interface to support virtual inputs/outputs with IPX800v4

---
+++ IPX800cs/IIPX800v4.cs
@@ -0,0 +1,49 @@
+﻿using software.elendil.IPX800.Enum;
+
+namespace software.elendil.IPX800
+{
+    public interface IIPX800v4 : IIPX800
+    {
+        /// <summary>
+        /// Gets the state of a virtual input
+        /// </summary>
+        /// <param name="inputNumber">The virtual input number.</param>
+        /// <returns>The input state.</returns>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800Exception">Thrown if it was unable to  get a response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
+        InputState GetVirtualIn(uint inputNumber);
+        
+        /// <summary>
+        /// Gets the state of a virtual output
+        /// </summary>
+        /// <param name="outputNumber">The virtual output number.</param>
+        /// <returns>The output state.</returns>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800Exception">Thrown if it was unable to  get a response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
+        OutputState GetVirtualOut(uint outputNumber);
+        
+        /// <summary>
+        /// Sets the state of a virtual output.
+        /// </summary>
+        /// <param name="outputNumber">The virtual output number.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="fugitive">if set to <c>true</c>, the 'fugitive' mode will be used.</param>
+        /// <returns><c>true</c> if successful, <c>false</c> otherwise.</returns>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800Exception">Thrown if it was unable to  get a response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
+        string SetVirtualOut(uint outputNumber, OutputState state, bool fugitive);
+        
+        /// <summary>
+        /// Gets the value of a virtual analog input
+        /// </summary>
+        /// <param name="inputNumber">The virtual input number.</param>
+        /// <returns>The value</returns>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800Exception">Thrown if it was unable to  get a response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
+        /// <exception cref="software.elendil.IPX800.Exceptions.IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
+        string GetVirtualAn(uint inputNumber);
+    }
+}
