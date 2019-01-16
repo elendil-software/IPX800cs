41c32e8cce12fa2704d1394199849b6ddbf7c5a2
Julien Tschäppät <git@elendil.software>
2019-01-16 19:09:43 +0100

    Added Exception classes

---
+++ IPX800cs/Exceptions/IPX800Exception.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace software.elendil.IPX800.Exceptions
+{
+	/// <summary>
+	/// IPX800 exception base class
+	/// </summary>
+	public class IPX800Exception : Exception
+	{
+        public IPX800Exception() { }
+
+		public IPX800Exception(string message) : base(message) { }
+
+		public IPX800Exception(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
---
+++ IPX800cs/Exceptions/IPX800UnknownVersionException.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace software.elendil.IPX800.Exceptions
+{
+	/// <summary>
+	/// Exception that can be thrown when the firmware version can not be identified
+	/// </summary>
+	public class IPX800UnknownVersionException : IPX800Exception
+	{
+        public IPX800UnknownVersionException() { }
+
+		public IPX800UnknownVersionException(string message) : base(message) { }
+
+		public IPX800UnknownVersionException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
