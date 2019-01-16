6fa85495bd34ecf72916fdd5e13a23ecf1566d9c
Julien Tschäppät <git@elendil.software>
2019-01-16 19:10:03 +0100

    Added Version enum

---
+++ IPX800cs/Version/IPX800Version.cs
@@ -0,0 +1,12 @@
+﻿namespace software.elendil.IPX800.Enum
+{
+	/// <summary>
+	/// Represents the hardware version of an IPX800 device (not the firmware version) 
+	/// </summary>
+	public enum IPX800Version
+	{
+		V4,
+		V3,
+		V2
+	}
+}
