f7ee129a70a02deb76a868a013afec7faa68753c
Julien Tschäppät <git@elendil.software>
2019-01-17 20:49:30 +0100

    Added IPX800 Context class

---
+++ IPX800cs/Context.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using software.elendil.IPX800.Version;
+
+namespace software.elendil.IPX800
+{
+    public class Context
+    {
+        public Context(string ip, int port, IPX800Protocol protocol, IPX800Version version)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                throw new ArgumentNullException(nameof(ip));
+            }
+
+            IP = IPAddress.Parse(ip);
+            Port = port;
+            Protocol = protocol;
+            Version = version;
+        }
+
+        public Context(string ip, int port, IPX800Protocol protocol, IPX800Version version, System.Version firmwareVersion)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                throw new ArgumentNullException(nameof(ip));
+            }
+
+            IP = IPAddress.Parse(ip);
+            Port = port;
+            Protocol = protocol;
+            Version = version;
+            FirmwareVersion = firmwareVersion ?? throw new ArgumentNullException(nameof(firmwareVersion));
+        }
+
+        public Context(string ip, int port, IPX800Protocol protocol, IPX800Version version, string user, string password)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                throw new ArgumentNullException(nameof(ip));
+            }
+
+            IP = IPAddress.Parse(ip);
+            Port = port;
+            Protocol = protocol;
+            Version = version;
+            User = user;
+            Password = password;
+        }
+
+        public Context(string ip, int port, IPX800Protocol protocol, IPX800Version version, string user,
+            string password, System.Version firmwareVersion)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                throw new ArgumentNullException(nameof(ip));
+            }
+
+            IP = IPAddress.Parse(ip);
+            Port = port;
+            Protocol = protocol;
+            Version = version;
+            User = user ?? throw new ArgumentNullException(nameof(user));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+            FirmwareVersion = firmwareVersion ?? throw new ArgumentNullException(nameof(firmwareVersion));
+        }
+
+        public IPX800Version Version { get; }
+        public System.Version FirmwareVersion { get; }
+        public IPX800Protocol Protocol { get; }
+        public IPAddress IP { get; }
+        public int Port { get; }
+        public string User { get; }
+        public string Password { get; }
+    }
+}
