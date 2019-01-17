a4d8df1a7c9f08d79445df93b1d9913be1ddcb0d
Julien Tschäppät <git@elendil.software>
2019-01-17 20:48:30 +0100

    Added command builders

---
+++ IPX800cs/Commands/Builders/IGetInCommandBuilder.cs
@@ -0,0 +1,9 @@
+﻿using software.elendil.IPX800.IO;
+
+namespace software.elendil.IPX800.Commands.Builders
+{
+    public interface IGetInCommandBuilder
+    {
+        string BuildCommandString(IPX800Input input);
+    }
+}
