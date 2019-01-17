4c7078657835975f93a7f562ffca6b8a45013305
Julien Tschäppät <git@elendil.software>
2019-01-17 07:30:53 +0100

    Added base of the IPX800 implementation

---
+++ IPX800cs/IPX800.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using software.elendil.IPX800.Enum;
+using software.elendil.IPX800.IO;
+
+namespace software.elendil.IPX800
+{
+    public class IPX800 : IIPX800v4
+    {
+        private IPX800Version _version;
+        private Version firmwareVersion;
+        private IPX800Protocol _protocol;
+
+        public InputState GetIn(uint inputNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutputState GetOut(uint outputNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetOut(uint outputNumber, OutputState state, bool fugitive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAn(uint inputNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public InputState GetVirtualIn(uint inputNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutputState GetVirtualOut(uint outputNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetVirtualOut(uint outputNumber, OutputState state, bool fugitive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetVirtualAn(uint inputNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
