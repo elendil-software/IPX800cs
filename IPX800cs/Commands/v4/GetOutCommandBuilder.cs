46a4f84ec600ce224706b74eff25e6cc7a94e7fa
Julien Tschäppät <git@elendil.software>
2019-01-17 07:21:21 +0100

    Added IPX800 v4 commands builders

---
+++ IPX800cs/Commands/v4/GetOutCommandBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace software.elendil.IPX800.Commands.v4.M2M
+{
+    public class GetOutCommandBuilder
+    {
+        protected string BuildCommandString(int inputNumber, bool isVirtual)
+        {
+            return "Get=R";
+        }
+    }
+}
---
+++ IPX800cs/Commands/v4/GetVirtualAnalogInputCommandBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace software.elendil.IPX800.Commands.v4.M2M
+{
+    public class GetVirtualAnalogInputCommandBuilder
+    {
+        protected string BuildCommandString(int inputNumber, bool isVirtual)
+        {
+            return "Get=VA";
+        }
+    }
+}
