1720c9f87f25dc03b284a43bda71871a97313e65
Julien Tschäppät <git@elendil.software>
2019-01-17 20:48:50 +0100

    Added command builder factory

---
+++ IPX800cs/Commands/Builders/ICommandBuilderFactory.cs
@@ -0,0 +1,11 @@
+﻿using software.elendil.IPX800.IO;
+
+namespace software.elendil.IPX800.Commands.Builders
+{
+    public interface ICommandBuilderFactory
+    {
+        ISetCommandBuilder GetSetOutCommandBuilder(Context context, IPX800Output output);
+        IGetOutCommandBuilder GetGetOutputCommandBuilder(Context context, IPX800Output input);
+        IGetInCommandBuilder GetGetInputCommandBuilder(Context context, IPX800Input input);
+    }
+}
