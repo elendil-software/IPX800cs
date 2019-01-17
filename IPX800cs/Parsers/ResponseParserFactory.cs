cf9ab9dd787e6f6c0e4a2aa8244f6574e2e2c089
Julien Tschäppät <git@elendil.software>
2019-01-17 20:41:44 +0100

    Begin of ResponseParserFactory

---
+++ IPX800cs/Parsers/ResponseParserFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using software.elendil.IPX800.IO;
+using software.elendil.IPX800.Parsers.v4.M2M;
+using software.elendil.IPX800.Version;
+
+namespace software.elendil.IPX800.Parsers
+{
+    public class ResponseParserFactory : IResponseParserFactory
+    {
+        public IInputResponseParser<InputState> GetInputParser(Context context, IPX800Input input)
+        {
+            if (context.Version == IPX800Version.V4)
+            {
+                if (input.Type == InputType.DigitalInput)
+                {
+                    if (input.IsVirtual)
+                    {
+                        return new VirtualInputResponseParser();
+                    }
+                    else
+                    {
+                        return new InputResponseParser();
+                    }
+                }
+            }
+
+            throw new Exception("Invalid context or input");
+        }
+    }
+}
