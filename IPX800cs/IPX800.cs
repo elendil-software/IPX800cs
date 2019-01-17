a0d59b36f037f6fd16e580e53e3d9be480c506d5
Julien Tschäppät <git@elendil.software>
2019-01-17 20:50:43 +0100

    IPX800 implementation

--- IPX800cs/IPX800.cs
+++ IPX800cs/IPX800.cs
@@ -1,51 +1,97 @@
 ﻿using System;
-using software.elendil.IPX800.Enum;
+using software.elendil.IPX800.Commands.Builders;
+using software.elendil.IPX800.Commands.Senders;
+using software.elendil.IPX800.ipx800csV1.Exceptions;
 using software.elendil.IPX800.IO;
+using software.elendil.IPX800.Parsers;
+using software.elendil.IPX800.Version;
+using IPX800ConnectionException = software.elendil.IPX800.Exceptions.IPX800ConnectionException;
+using IPX800Exception = software.elendil.IPX800.Exceptions.IPX800Exception;
 
 namespace software.elendil.IPX800
 {
     public class IPX800 : IIPX800v4
     {
-        private IPX800Version _version;
-        private Version firmwareVersion;
-        private IPX800Protocol _protocol;
+        private readonly Context context;
+        private readonly ICommandBuilderFactory commandBuilderFactory;
+        private readonly ICommandSender commandSender;
+        private readonly IResponseParserFactory responseParserFactory;
 
-        public InputState GetIn(uint inputNumber)
+        public IPX800()
+        {
+            context = new Context("192.168.1.130", 9870, IPX800Protocol.M2M, IPX800Version.V4, "", "");
+            commandBuilderFactory = new CommandBuilderFactory();
+            ICommandSenderFactory commandSenderFactory = new CommandSenderFactory();
+            commandSender = commandSenderFactory.GetCommandSender(context);
+            responseParserFactory = new ResponseParserFactory();
+        }
+
+        public InputState GetIn(int inputNumber)
+        {
+            var input = new IPX800Input()
+            {
+                IsVirtual = false,
+                Number = inputNumber,
+                Type = InputType.DigitalInput
+            };
+
+            string response = null;
+            string command = null;
+
+            try
+            {
+                IGetInCommandBuilder commandBuilder = commandBuilderFactory.GetGetInputCommandBuilder(context, input);
+                command = commandBuilder.BuildCommandString(input);
+
+                response = commandSender.ExecuteCommand(command);
+                InputState result = responseParserFactory.GetInputParser(context, input).ParseResponse(response, inputNumber);
+                return result;
+            }
+            catch (IPX800ConnectionException)
+            {
+                throw;
+            }
+            catch (IPX800ExecuteException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new IPX800Exception($"Unable to parse response '{response}' for command '{command}'", e);
+            }
+        }
+
+        public OutputState GetOut(int outputNumber)
         {
             throw new NotImplementedException();
         }
 
-        public OutputState GetOut(uint outputNumber)
+        public string SetOut(int outputNumber, OutputState state, bool fugitive)
         {
             throw new NotImplementedException();
         }
 
-        public string SetOut(uint outputNumber, OutputState state, bool fugitive)
+        public string GetAn(int inputNumber)
         {
             throw new NotImplementedException();
         }
 
-        public string GetAn(uint inputNumber)
+        public InputState GetVirtualIn(int inputNumber)
         {
             throw new NotImplementedException();
         }
 
-        public InputState GetVirtualIn(uint inputNumber)
+        public OutputState GetVirtualOut(int outputNumber)
         {
             throw new NotImplementedException();
         }
 
-        public OutputState GetVirtualOut(uint outputNumber)
+        public string SetVirtualOut(int outputNumber, OutputState state, bool fugitive)
         {
             throw new NotImplementedException();
         }
 
-        public string SetVirtualOut(uint outputNumber, OutputState state, bool fugitive)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetVirtualAn(uint inputNumber)
+        public string GetVirtualAn(int inputNumber)
         {
             throw new NotImplementedException();
         }
