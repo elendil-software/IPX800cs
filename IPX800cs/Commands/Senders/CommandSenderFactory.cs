d716d36fde2677d090957cf886286d080c5905f1
Julien Tschäppät <git@elendil.software>
2019-01-17 20:50:08 +0100

    Added command senders

---
+++ IPX800cs/Commands/Senders/CommandSenderFactory.cs
@@ -0,0 +1,23 @@
+﻿using software.elendil.IPX800.Version;
+
+namespace software.elendil.IPX800.Commands.Senders
+{
+    public class CommandSenderFactory : ICommandSenderFactory
+    {
+        public ICommandSender GetCommandSender(Context context)
+        {
+            if (context.Protocol == IPX800Protocol.M2M)
+            {
+                return new CommandSenderM2M(context);
+            }
+            else if (context.Version == IPX800Version.V4)
+            {
+                return new CommandSenderHttpIPX800V4(context);
+            }
+            else
+            {
+                return new CommandSenderHttp(context);
+            }
+        }
+    }
+}
---
+++ IPX800cs/Commands/Senders/ICommandSender.cs
@@ -0,0 +1,19 @@
+﻿using software.elendil.IPX800.ipx800csV1.Exceptions;
+
+namespace software.elendil.IPX800.Commands.Senders
+{
+	/// <summary>
+	/// Interface ICommandSender. Provide method to send command to an IPX800
+	/// </summary>
+	public interface ICommandSender
+	{
+        /// <summary>
+        /// Execute the given command
+        /// </summary>
+        /// <param name="command">the command to execute</param>
+        /// <returns>the response to the executed</returns>
+        /// <exception cref="IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
+        /// <exception cref="IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
+        string ExecuteCommand(string command);
+	}
+}
