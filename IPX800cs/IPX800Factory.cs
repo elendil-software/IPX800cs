507a43c44ce273d2d7bed30a001d75b1ae53ac04
Julien Tschäppät <git@elendil.software>
2019-01-18 15:43:44 +0100

    Added version checker and IPX800 factory

---
+++ IPX800cs/IPX800Factory.cs
@@ -0,0 +1,101 @@
+﻿using software.elendil.IPX800.Exceptions;
+using software.elendil.IPX800.Version;
+
+namespace software.elendil.IPX800
+{
+	public class IPX800Factory
+	{
+		/// <summary>
+		/// Gets the IPX800 implementation by guess. 
+		/// First it try to get the IPX800 firmware version and the corresponding implementation. 
+		/// If it fails <see cref="TryToGuess"/> is called
+		/// </summary>
+		/// <param name="ip">The ip</param>
+		/// <param name="port">The port</param>
+		/// <param name="ipx800Version">The version of the IPX800</param>
+		/// <param name="pass">password</param>
+		/// <returns>An IIPX800 implementation.</returns>
+		/// <exception cref="IPX800UnknownVersionException">Thrown if unable to guess the version</exception>
+		
+		public static IIPX800 GetInstance(string ip, int port, IPX800Protocol protocol, IPX800Version ipx800Version, string user, string password)
+        {
+            Context context = new Context(ip, port, protocol, ipx800Version, user, password);
+
+            if (ipx800Version == IPX800Version.V3)
+            {
+                System.Version firmwareVersion = VersionChecker.GetVersion(context);
+
+                if (firmwareVersion != null)
+                {
+                    context = new Context(ip, port, protocol, ipx800Version, user, password, firmwareVersion);
+                }
+                else
+                {
+                    context = TryToGuess(context);
+                }
+            }
+
+            return new IPX800(context);
+		}
+        
+		/// <summary>
+		/// Gets the instance by guess. It try to read the first output state to identify the appropriate implementation
+		/// </summary>
+		/// <param name="ip">The ip</param>
+		/// <param name="port">The port</param>
+		/// <param name="pass">password</param>
+		/// <returns>An IIPX800 implementation.</returns>
+		/// <exception cref="IPX800UnknownVersionException">Unable to guess the version</exception>
+		private static Context TryToGuess(Context context)
+		{
+            if (IsLegacy(context))
+            {
+                return new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3,5,38)); 
+            }
+            else if (Is30542OrNewer(context))
+            {
+                return new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3, 5, 42));
+            }
+			else
+			{
+				throw new IPX800UnknownVersionException("Unable to guess the version");
+			}
+		}
+
+        /// <summary>
+        /// Try implementation for firmware older than 3.05.42
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private static bool IsLegacy(Context context)
+        {
+            try
+            {
+                //Version 3.05.38 is the version released before the 3.05.42
+                Context version30538Context = new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3,5,38));
+                var ipx800 = new IPX800(version30538Context);
+                ipx800.GetOut(1);
+                return true;
+            }
+            catch (IPX800Exception)
+            {
+                return false;
+            }
+        }
+
+        private static bool Is30542OrNewer(Context context)
+        {
+            try
+            {
+                Context version30542Context = new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3, 5, 42));
+                var ipx800 = new IPX800(version30542Context);
+                ipx800.GetOut(1);
+                return true;
+            }
+            catch (IPX800Exception)
+            {
+                return false;
+            }
+        }
+	}
+}
---
+++ IPX800cs/Version/VersionChecker.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using software.elendil.IPX800.Commands.Senders;
+using software.elendil.IPX800.ipx800csV1.Exceptions;
+
+namespace software.elendil.IPX800.Version
+{
+	/// <summary>
+	/// Class VersionChecker.
+	/// </summary>
+	public static class VersionChecker
+	{
+		/// <summary>
+		/// Gets the version of the IPX800 firmware
+		/// </summary>
+		/// <param name="ip">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <param name="pass">password (if configured in the IPX)</param>
+		/// <returns> the version of the IPX800 firmware</returns>
+		public static System.Version GetVersion(Context context)
+		{
+			var commandSender = new CommandSenderM2M(context);
+			var versionString = (string) commandSender.ExecuteCommand("GetVersion");
+			var version = ParseVersion(versionString);
+			return version;
+		}
+
+		/// <summary>
+		/// Gets the version by HTTP.
+		/// </summary>
+		/// <param name="ip">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <param name="user">The user.</param>
+		/// <param name="pass">The pass.</param>
+		/// <returns>System.Version.</returns>
+		public static System.Version GetVersionByHttp(Context context)
+		{
+			var commandSender = new CommandSenderHttp(context);
+			string versionString;
+
+			try
+			{
+				var response = commandSender.ExecuteCommand("globalstatus.xml");
+				XDocument xmlDoc = XDocument.Load(response);
+				versionString = xmlDoc.Element("response").Elements("version").FirstOrDefault().Value;
+			}
+			catch (IPX800ConnectionException connectionException)
+			{
+				if (connectionException.InnerException != null &&
+				    connectionException.InnerException.GetType() == typeof (WebException))
+				{
+					var webException = (WebException) connectionException.InnerException;
+					var response = (HttpWebResponse) webException.Response;
+
+					if (response != null && response.StatusCode.Equals(HttpStatusCode.NotFound))
+					{
+						versionString = GetVersionByHttpLegacy(context);
+					}
+					else
+					{
+						throw;
+					}
+				}
+				else
+				{
+					throw;
+				}
+			}
+			catch (Exception)
+			{
+				versionString = GetVersionByHttpLegacy(context);
+			}
+
+			var version = ParseVersion(versionString);
+			return version;
+		}
+
+		/// <summary>
+		/// Gets the version by HTTP legacy.
+		/// </summary>
+		/// <param name="ip">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <param name="user">The user.</param>
+		/// <param name="pass">The pass.</param>
+		/// <returns>System.String.</returns>
+		private static string GetVersionByHttpLegacy(Context context)
+		{
+			var commandSender = new CommandSenderHttp(context);
+			string versionString;
+
+			try
+			{
+				var response = commandSender.ExecuteCommand("status.xml");
+				XDocument xmlDoc = XDocument.Load(response);
+				versionString = xmlDoc.Element("response").Elements("version").FirstOrDefault().Value;
+			}
+			catch (IPX800ConnectionException connectionException)
+			{
+				if (connectionException.InnerException != null &&
+				    connectionException.InnerException.GetType() == typeof (WebException))
+				{
+					var webException = (WebException) connectionException.InnerException;
+					var response = (HttpWebResponse) webException.Response;
+
+					if (response.StatusCode.Equals(HttpStatusCode.NotFound))
+					{
+						versionString = "UNKNOWN";
+					}
+					else
+					{
+						throw;
+					}
+				}
+				else
+				{
+					throw;
+				}
+			}
+			catch (Exception)
+			{
+				versionString = "UNKNOWN";
+			}
+
+			return versionString;
+		}
+
+		/// <summary>
+		/// Parses the version.
+		/// </summary>
+		/// <param name="input">The input.</param>
+		/// <returns>Version instance, <c>null</c> if unable to parse the input string</returns>
+		private static System.Version ParseVersion(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				return null;
+			}
+
+			input = input.Replace("GetVersion=", "").Trim();
+			input = new Regex(@"[A-Za-z]").Replace(input, "");
+
+			System.Version version;
+			return System.Version.TryParse(input, out version) ? version : null;
+		}
+	}
+}
