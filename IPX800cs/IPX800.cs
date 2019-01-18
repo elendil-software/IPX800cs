852ea0bbadad50a3fcd46cb6016bf99aed545c7f
Julien Tschäppät <git@elendil.software>
2019-01-18 15:43:18 +0100

    Removed default constructor

--- IPX800cs/IPX800.cs
+++ IPX800cs/IPX800.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using software.elendil.IPX800.Commands.Builders;
 using software.elendil.IPX800.Commands.Senders;
-using software.elendil.IPX800.ipx800csV1.Exceptions;
 using software.elendil.IPX800.IO;
 using software.elendil.IPX800.Parsers;
-using software.elendil.IPX800.Version;
-using IPX800ConnectionException = software.elendil.IPX800.Exceptions.IPX800ConnectionException;
 using IPX800Exception = software.elendil.IPX800.Exceptions.IPX800Exception;
 
 namespace software.elendil.IPX800
@@ -17,12 +14,11 @@ namespace software.elendil.IPX800
         private readonly ICommandSender commandSender;
         private readonly IResponseParserFactory responseParserFactory;
 
-        public IPX800()
+        public IPX800(Context context)
         {
-            context = new Context("192.168.1.130", 9870, IPX800Protocol.M2M, IPX800Version.V4, "", "");
-            commandBuilderFactory = new CommandBuilderFactory();
-            ICommandSenderFactory commandSenderFactory = new CommandSenderFactory();
-            commandSender = commandSenderFactory.GetCommandSender(context);
+            this.context = context;
+            commandBuilderFactory = new CommandBuilderFactoryProvider().GetCommandBuilderFactory(context);
+            commandSender = new CommandSenderFactory().GetCommandSender(context);
             responseParserFactory = new ResponseParserFactory();
         }
 
