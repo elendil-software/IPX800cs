674aab4882cc6e7123e164905c6e71b2a883264c
Julien Tschäppät <git@elendil.software>
2019-01-22 21:35:40 +0100

    Names uniformisation : renamed IPX800v3GetInputM2MCommandBuilder

--- IPX800cs.Test/Commands/Builders/v3/M2M/GetInputM2MCommandBuilderTest.cs
+++ IPX800cs.Test/Commands/Builders/v3/M2M/GetInputM2MCommandBuilderTest.cs
@@ -10,7 +10,7 @@ namespace IPX800cs.Test.Commands.Builders.v3.M2M
         public void BuildCommandString_Returns_CorrectCommandString()
         {
             //Arrange
-            var commandBuilder = new GetInCommandBuilder();
+            var commandBuilder = new IPX800v3GetInputM2MCommandBuilder();
             var input = new IPX800Input {Type = InputType.DigitalInput, Number = 2, IsVirtual = false};
             
             //Act
