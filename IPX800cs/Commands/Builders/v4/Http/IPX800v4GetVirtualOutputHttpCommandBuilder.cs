e03cb86eb613ec341a73009c602ab784211e5bba
Julien Tschäppät <git@elendil.software>
2019-01-22 21:41:47 +0100

    Names uniformisation : renamed IPX800v4GetVirtualOutputHttpCommandBuilder

--- IPX800cs/Commands/Builders/v4/Http/IPX800v4GetVirtualOutputHttpCommandBuilder.cs
+++ IPX800cs/Commands/Builders/v4/Http/IPX800v4GetVirtualOutputHttpCommandBuilder.cs
@@ -2,7 +2,7 @@ using software.elendil.IPX800.IO;
 
 namespace software.elendil.IPX800.Commands.Builders.v4.Http
 {
-    public class GetVirtualOutHttpCommandBuilder : HttpCommandBuilderBase, IGetOutCommandBuilder
+    public class IPX800v4GetVirtualOutputHttpCommandBuilder : HttpCommandBuilderBase, IGetOutCommandBuilder
     {
         public string BuildCommandString(IPX800Output output)
         {
