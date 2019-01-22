82a1458531f18733ba2f33cdcf0f5037fbf549f7
Julien Tschäppät <git@elendil.software>
2019-01-22 08:18:38 +0100

    Correction IPX800 v2 M2M GetOutput command builder

--- IPX800cs/Commands/Builders/v2/M2M/IPX800v2M2MCommandStrings.cs
+++ IPX800cs/Commands/Builders/v2/M2M/IPX800v2M2MCommandStrings.cs
@@ -4,7 +4,7 @@ namespace software.elendil.IPX800.Commands.Builders.v2.M2M
     {
         public const string SetOutput = "Set";
         public const string SetOutputDelayedSuffix = "F";
-        public const string GetOutput = "status.xml";
+        public const string GetOutput = "GetOut";
         public const string GetDigitalInput = "GetIn";
         public const string GetAnalogInput = "GetAn";
     }
