08f32b3d411febc19820e9f8470e099d1308fa1a
Julien Tschäppät <git@elendil.software>
2019-01-22 21:39:37 +0100

    Names uniformisation : renamed IPX800v4GetInputHttpCommandBuilder

--- IPX800cs.Test/Commands/Builders/v4/V4HttpCommandBuilderFactoryTest.cs
+++ IPX800cs.Test/Commands/Builders/v4/V4HttpCommandBuilderFactoryTest.cs
@@ -24,7 +24,7 @@ namespace IPX800cs.Test.Commands.Builders.v4
             {
                 new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V4),
                 new IPX800Input { Number = 2, Type = InputType.DigitalInput},
-                typeof(GetInHttpCommandBuilder)
+                typeof(IPX800v4GetInputHttpCommandBuilder)
             },
             new object[]
             {
