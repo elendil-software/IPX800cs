edacc00697636181571ac34e57e779f7cc21820b
Julien Tschäppät <git@elendil.software>
2019-01-22 21:28:39 +0100

    Names uniformisation : renamed IPX800v3SetOutputHttpCommandBuilder

--- IPX800cs.Test/Commands/Builders/v3/V3HttpCommandBuilderFactoryTest.cs
+++ IPX800cs.Test/Commands/Builders/v3/V3HttpCommandBuilderFactoryTest.cs
@@ -109,7 +109,7 @@ namespace IPX800cs.Test.Commands.Builders.v3
             {
                 new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3),
                 new IPX800Output { Number = 2, Type = OutputType.Output},
-                typeof(SetOutHttpCommandBuilder)
+                typeof(IPX800v3SetOutputHttpCommandBuilder)
             }
         };
 
