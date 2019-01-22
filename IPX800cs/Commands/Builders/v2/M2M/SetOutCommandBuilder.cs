bd024cf087be45713be16a0735d12fc9b5e5adc8
Julien Tschäppät <git@elendil.software>
2019-01-22 08:27:20 +0100

    Correction of IPX800 v2 M2M SetOutput command builder

--- IPX800cs/Commands/Builders/v2/M2M/SetOutCommandBuilder.cs
+++ IPX800cs/Commands/Builders/v2/M2M/SetOutCommandBuilder.cs
@@ -7,13 +7,21 @@ namespace software.elendil.IPX800.Commands.Builders.v2.M2M
     {
         public string BuildCommandString(IPX800Output output)
         {
-            if (output.IsDelayed)
+            if (output.State == OutputState.Inactive)
             {
-                return $"{IPX800v2M2MCommandStrings.SetOutput}{output.Number}{IPX800v2M2MCommandStrings.SetOutputDelayedSuffix}";
+                return $"{IPX800v2M2MCommandStrings.SetOutput}{output.Number}{(int) output.State}";
             }
             else
             {
-                return $"{IPX800v2M2MCommandStrings.SetOutput}{output.Number}{(int) output.State}";
+                if (output.IsDelayed)
+                {
+                    return
+                        $"{IPX800v2M2MCommandStrings.SetOutput}{output.Number}{IPX800v2M2MCommandStrings.SetOutputDelayedSuffix}";
+                }
+                else
+                {
+                    return $"{IPX800v2M2MCommandStrings.SetOutput}{output.Number}{(int) output.State}";
+                }
             }
         }
     }
