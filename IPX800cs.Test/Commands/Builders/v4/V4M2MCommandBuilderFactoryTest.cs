f3dd2b8d925407e24b768f13b7e69c9bf53ce83a
Julien Tschäppät <git@elendil.software>
2019-01-22 18:40:28 +0100

    Unit tests for IPX800 v4 Command builder factories

---
+++ IPX800cs.Test/Commands/Builders/v4/V4M2MCommandBuilderFactoryTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using software.elendil.IPX800;
+using software.elendil.IPX800.Commands.Builders;
+using software.elendil.IPX800.Commands.Builders.v4;
+using software.elendil.IPX800.Commands.Builders.v4.M2M;
+using software.elendil.IPX800.IO;
+using software.elendil.IPX800.Version;
+using Xunit;
+
+namespace IPX800cs.Test.Commands.Builders.v4
+{
+    public class V4M2MCommandBuilderFactoryTest
+    {
+        public static IEnumerable<object[]> InputTestCases => new[]
+        {
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Input { Number = 2, Type = InputType.AnalogInput},
+                typeof(GetAnalogInputCommandBuilder)
+            },
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Input { Number = 2, Type = InputType.DigitalInput},
+                typeof(GetInCommandBuilder)
+            },
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Input { Number = 2, Type = InputType.VirtualAnalogInput, IsVirtual = true},
+                typeof(GetVirtualAnalogInputCommandBuilder)
+            },
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Input { Number = 2, Type = InputType.VirtualDigitalInput, IsVirtual = true},
+                typeof(GetVirtualInCommandBuilder)
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(InputTestCases))]
+        public void GetGetInputCommandBuilder_ReturnsCommandBuilder_CorrespondingToContextAndInput(Context context, IPX800Input input, Type type)
+        {
+            //Arrange
+            var v4M2MCommandBuilderFactory = new V4M2MCommandBuilderFactory();
+
+            //Act
+            IGetInCommandBuilder commandBuilderFactory = v4M2MCommandBuilderFactory.GetGetInputCommandBuilder(context, input);
+
+            //Assert
+            Assert.Equal(type, commandBuilderFactory.GetType());
+        }
+
+        public static IEnumerable<object[]> GetOutputTestCases => new[]
+        {
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Output { Number = 2, Type = OutputType.Output},
+                typeof(GetOutCommandBuilder)
+            },
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Output { Number = 2, Type = OutputType.VirtualOutput, IsVirtual = true},
+                typeof(GetVirtualOutCommandBuilder)
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(GetOutputTestCases))]
+        public void GetGetOutputCommandBuilder_ReturnsCommandBuilder_CorrespondingToContextAndInput(Context context, IPX800Output output, Type type)
+        {
+            //Arrange
+            var v4M2MCommandBuilderFactory = new V4M2MCommandBuilderFactory();
+
+            //Act
+            IGetOutCommandBuilder commandBuilderFactory = v4M2MCommandBuilderFactory.GetGetOutputCommandBuilder(context, output);
+
+            //Assert
+            Assert.Equal(type, commandBuilderFactory.GetType());
+        }
+
+        public static IEnumerable<object[]> SetOutputTestCases => new[]
+        {
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Output { Number = 2, Type = OutputType.Output},
+                typeof(SetOutCommandBuilder)
+            },
+            new object[]
+            {
+                new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V4),
+                new IPX800Output { Number = 2, Type = OutputType.VirtualOutput, IsVirtual = true},
+                typeof(SetVirtualOutCommandBuilder)
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(SetOutputTestCases))]
+        public void GetSetOutCommandBuilder_ReturnsCommandBuilder_CorrespondingToContextAndInput(Context context, IPX800Output output, Type type)
+        {
+            //Arrange
+            var v4M2MCommandBuilderFactory = new V4M2MCommandBuilderFactory();
+
+            //Act
+            ISetCommandBuilder commandBuilderFactory = v4M2MCommandBuilderFactory.GetSetOutCommandBuilder(context, output);
+
+            //Assert
+            Assert.Equal(type, commandBuilderFactory.GetType());
+        }
+    }
+}
