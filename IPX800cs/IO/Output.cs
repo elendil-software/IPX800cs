de43320fd5059472bb7bdc433d5d439d98d78fe2
Julien Tschäppät <git@elendil.software>
2019-01-23 17:34:48 +0100

    Removed virtual property from Output

--- IPX800cs/IO/Output.cs
+++ IPX800cs/IO/Output.cs
@@ -5,7 +5,6 @@
         public OutputType Type { get; set; }
         public OutputState State { get; set; }
         public int Number { get; set; }
-        public bool IsVirtual { get; set; }
         public bool IsDelayed { get; set; }
     }
 }
