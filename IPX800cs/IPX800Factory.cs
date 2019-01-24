69f2dba5a67d2e30ee5da12ab127eec9036483f5
Julien Tschäppät <git@elendil.software>
2019-01-24 20:01:20 +0100

    Refactoring of IPX800 factory

--- IPX800cs/IPX800Factory.cs
+++ IPX800cs/IPX800Factory.cs
@@ -23,79 +23,31 @@ namespace software.elendil.IPX800
 
             if (ipx800Version == IPX800Version.V3)
             {
-                System.Version firmwareVersion = VersionChecker.GetVersion(context);
-
-                if (firmwareVersion != null)
-                {
-                    context = new Context(ip, port, protocol, ipx800Version, user, password, firmwareVersion);
-                }
-                else
-                {
-                    context = TryToGuess(context);
-                }
+	            context = GetIPX800v3Context(ip, port, protocol, ipx800Version, user, password);
             }
 
             return new IPX800(context);
 		}
-        
-		/// <summary>
-		/// Gets the instance by guess. It try to read the first output state to identify the appropriate implementation
-		/// </summary>
-		/// <param name="ip">The ip</param>
-		/// <param name="port">The port</param>
-		/// <param name="pass">password</param>
-		/// <returns>An IIPX800 implementation.</returns>
-		/// <exception cref="IPX800UnknownVersionException">Unable to guess the version</exception>
-		private static Context TryToGuess(Context context)
+
+		private static Context GetIPX800v3Context(string ip, int port, IPX800Protocol protocol, IPX800Version ipx800Version, string user, string password)
 		{
-            if (IsLegacy(context))
-            {
-                return new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3,5,38)); 
-            }
-            else if (Is30542OrNewer(context))
-            {
-                return new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3, 5, 42));
-            }
-			else
+			try
 			{
-				throw new IPX800UnknownVersionException("Unable to guess the version");
+				//by setting no firmware version, will consider it's >= 3.05.42
+				Context context = new Context(ip, port, protocol, ipx800Version, user, password);
+				IIPX800 ipx800 = new IPX800(context);
+				System.Version firmwareVersion = ipx800.GetVersion();
+				return new Context(ip, port, protocol, ipx800Version, user, password, firmwareVersion);
+			}
+			//if exception is thrown, we didn't try with the correct version and should by <= 3.05.38
+			catch (IPX800UnknownVersionException)
+			{
+				//Version 3.05.38 is the version released before the 3.05.42
+				Context context =  new Context(ip, port, protocol, ipx800Version, user, password, new System.Version(3, 5, 38));
+				IIPX800 ipx800 = new IPX800(context);
+				System.Version firmwareVersion = ipx800.GetVersion();
+				return new Context(ip, port, protocol, ipx800Version, user, password, firmwareVersion);
 			}
 		}
-
-        /// <summary>
-        /// Try implementation for firmware older than 3.05.42
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        private static bool IsLegacy(Context context)
-        {
-            try
-            {
-                //Version 3.05.38 is the version released before the 3.05.42
-                Context version30538Context = new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3,5,38));
-                var ipx800 = new IPX800(version30538Context);
-                ipx800.GetOut(1);
-                return true;
-            }
-            catch (IPX800Exception)
-            {
-                return false;
-            }
-        }
-
-        private static bool Is30542OrNewer(Context context)
-        {
-            try
-            {
-                Context version30542Context = new Context(context.IP.ToString(), context.Port, context.Protocol, context.Version, "", context.Password, new System.Version(3, 5, 42));
-                var ipx800 = new IPX800(version30542Context);
-                ipx800.GetOut(1);
-                return true;
-            }
-            catch (IPX800Exception)
-            {
-                return false;
-            }
-        }
 	}
 }
