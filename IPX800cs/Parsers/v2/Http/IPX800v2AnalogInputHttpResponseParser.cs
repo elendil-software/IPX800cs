ec8769512df762ba8e2e0faad67613c81ede6ada
Julien Tschäppät <git@elendil.software>
2019-01-26 16:08:28 +0100

    Added IPX800v2 Http responses parsers

---
+++ IPX800cs/Parsers/v2/Http/IPX800v2AnalogInputHttpResponseParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace software.elendil.IPX800.Parsers.v2.Http
+{
+    public class IPX800v2AnalogInputHttpResponseParser : IAnalogInputResponseParser
+    {
+        public double ParseResponse(string ipxResponse, int inputNumber)
+        {
+            XDocument xmlDoc = XDocument.Parse(ipxResponse);
+            string value = xmlDoc.Element("response").Elements($"an{inputNumber}").First().Value;
+            return double.Parse(value);
+        }
+    }
+}
---
+++ IPX800cs/Parsers/v2/Http/IPX800v2SetOutputHttpResponseParser.cs
@@ -0,0 +1,10 @@
+﻿namespace software.elendil.IPX800.Parsers.v2.Http
+{
+    public class IPX800v2SetOutputHttpResponseParser : ISetOutputResponseParser
+    {
+        public bool ParseResponse(string ipxResponse)
+        {
+            return ipxResponse?.Trim() == "OK";
+        }
+    }
+}
