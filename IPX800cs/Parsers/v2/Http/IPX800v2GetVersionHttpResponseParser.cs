9a9d4146e17a43e41f2c453b8dee2132dc62a48b
Julien Tschäppät <git@elendil.software>
2019-01-28 18:09:40 +0100

    Added IPX800 v2 GetVersion Http Parser

---
+++ IPX800cs/Parsers/v2/Http/IPX800v2GetVersionHttpResponseParser.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Xml.Linq;
+
+namespace software.elendil.IPX800.Parsers.v2.Http
+{
+    public class IPX800v2GetVersionHttpResponseParser : IGetVersionResponseParser
+    {
+        public string ParseResponse(string ipxResponse)
+        {
+            XDocument xmlDoc = XDocument.Parse(ipxResponse);
+            return xmlDoc.Element("response").Elements("firmware_version").First().Value;
+        }
+    }
+}
