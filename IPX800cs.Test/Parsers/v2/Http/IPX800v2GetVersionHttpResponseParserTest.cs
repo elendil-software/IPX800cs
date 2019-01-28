9ef0987dfc100b21a4435177ccea0113a1fc34f5
Julien Tschäppät <git@elendil.software>
2019-01-28 18:10:00 +0100

    Added IPX800 v2 GetVersion Http Parser Unit test

---
+++ IPX800cs.Test/Parsers/v2/Http/IPX800v2GetVersionHttpResponseParserTest.cs
@@ -0,0 +1,21 @@
+using software.elendil.IPX800.Parsers.v2.Http;
+using Xunit;
+
+namespace IPX800cs.Test.Parsers.v2.Http
+{
+    public class IPX800v2GetVersionHttpResponseParserTest
+    {
+        [Fact]
+        public void GivenValidResponse_ParseResponse_ReturnsFirmwareVersion()
+        {
+            //Arrange
+            var parser = new IPX800v2GetVersionHttpResponseParser();
+
+            //Act
+            string firmwareVersion = parser.ParseResponse(IPX800v2HttpResponse.Xml);
+            
+            //Assert
+            Assert.Equal("3.00.29", firmwareVersion);
+        }
+    }
+}
