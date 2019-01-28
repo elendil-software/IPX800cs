e5a8ed50fc87777b205c10b6b5409e718a662ca9
Julien Tschäppät <git@elendil.software>
2019-01-28 17:38:59 +0100

    Added IPX800 v3 M2M Parsers

---
+++ IPX800cs/Parsers/v3/M2M/IPX800v3GetInputM2MResponseParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using software.elendil.IPX800.IO;
+
+namespace software.elendil.IPX800.Parsers.v3.M2M
+{
+    public class IPX800v3GetInputM2MResponseParser : IInputResponseParser
+    {
+        public InputState ParseResponse(string ipxResponse, int inputNumber)
+        {
+            var result = ipxResponse.Trim();
+            return (InputState)Enum.Parse(typeof(InputState), result);
+        }
+    }
+}
