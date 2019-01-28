e445f260090ba228cd16738ceeecc1b9b75c4f74
Julien Tschäppät <git@elendil.software>
2019-01-28 17:38:31 +0100

    Added IPX800 v2 M2M Parsers

---
+++ IPX800cs/Parsers/v2/M2M/IPX800v2GetInputM2MResponseParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using software.elendil.IPX800.IO;
+
+namespace software.elendil.IPX800.Parsers.v2.M2M
+{
+    public class IPX800v2GetInputM2MResponseParser : IInputResponseParser
+    {
+        public InputState ParseResponse(string ipxResponse, int inputNumber)
+        {
+            var result = ipxResponse.Trim().Split('=')[1];
+            return (InputState)Enum.Parse(typeof(InputState), result);
+        }
+    }
+}
