99c75247b21190ef92491a872e0ed40151e7708e
Julien Tschäppät <git@elendil.software>
2019-01-28 17:39:20 +0100

    Added IPX800 v3 Legacy M2M Parsers

---
+++ IPX800cs/Parsers/v3/Legacy/M2M/IPX800v3LegacyGetOutputM2MResponseParser.cs
@@ -0,0 +1,13 @@
+﻿using software.elendil.IPX800.IO;
+
+namespace software.elendil.IPX800.Parsers.v3.Legacy.M2M
+{
+    public class IPX800v3LegacyGetOutputM2MResponseParser : IGetOutputResponseParser
+    {
+        public OutputState ParseResponse(string ipxResponse, int outputNumber)
+        {
+            string result = ipxResponse.Trim().Split('=')[1];
+            return (OutputState)System.Enum.Parse(typeof(OutputState), result);
+        }
+    }
+}
