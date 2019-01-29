52f99bbbcf86d556004d2bc8ff528a641b933c2f
Julien Tschäppät <git@elendil.software>
2019-01-29 20:02:14 +0100

    Removed unused code

--- IPX800cs/Parsers/v4/M2M/NonHeadedResponseParserBase.cs
+++ IPX800cs/Parsers/v4/M2M/NonHeadedResponseParserBase.cs
@@ -1,8 +1,4 @@
-﻿using System.Linq;
-using System.Text.RegularExpressions;
-using software.elendil.IPX800.Exceptions;
-
-namespace software.elendil.IPX800.Parsers.v4.M2M
+﻿namespace software.elendil.IPX800.Parsers.v4.M2M
 {
     public abstract class ResponseParserBase
     {
@@ -22,20 +18,6 @@ namespace software.elendil.IPX800.Parsers.v4.M2M
             }
 
             return result;
-        }
-
-        protected string ExtractMatchingValue(string responseString, int inputOutputNumber, string pattern)
-        {
-            var matches = Regex.Matches(responseString, pattern);
-
-            if (matches.Count == 1)
-            {
-                Match match = matches.OfType<Match>().ToArray().ElementAt(0);
-                var result = match.Groups[1].Value;
-                return result;
-            }
-
-            throw new IPX800CommandException($"Unable to parse '{responseString}' response string");
         }
     }
 }
