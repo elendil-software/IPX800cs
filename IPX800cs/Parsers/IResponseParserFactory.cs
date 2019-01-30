12a30cc58db041c36405a8317f35409dd4a839dc
Julien Tschäppät <git@elendil.software>
2019-01-30 20:43:28 +0100

    Added responses parsers factories

--- IPX800cs/Parsers/IResponseParserFactory.cs
+++ IPX800cs/Parsers/IResponseParserFactory.cs
@@ -4,6 +4,10 @@ namespace software.elendil.IPX800.Parsers
 {
     public interface IResponseParserFactory
     {
+        IGetVersionResponseParser GetVersionResponseParser(Context context);
+        IAnalogInputResponseParser GetAnalogInputParser(Context context, Input input);
         IInputResponseParser GetInputParser(Context context, Input input);
+        IGetOutputResponseParser GetOutputParser(Context context, Output input);
+        ISetOutputResponseParser GetSetOutputParser(Context context, Output input);
     }
 }
---
+++ IPX800cs/Parsers/v4/IPX800v4ResponseParserFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using software.elendil.IPX800.Exceptions;
+using software.elendil.IPX800.IO;
+using software.elendil.IPX800.Parsers.v4.Http;
+using software.elendil.IPX800.Parsers.v4.M2M;
+
+namespace software.elendil.IPX800.Parsers.v4
+{
+    public class IPX800v4ResponseParserFactory : IResponseParserFactory
+    {
+        public IGetVersionResponseParser GetVersionResponseParser(Context context)
+        {
+            throw new IPX800NotSupportedCommandException("GetVersion command is not supported by IPX800v4");
+        }
+
+        public IAnalogInputResponseParser GetAnalogInputParser(Context context, Input input)
+        {
+            switch (context.Protocol)
+            {
+                case IPX800Protocol.Http:
+                    return new IPX800v4GetAnalogInputHttpResponseParser();
+                
+                case IPX800Protocol.M2M:
+                    return new IPX800v4GetAnalogInputM2MResponseParser();
+                
+                default:
+                    throw new IPX800InvalidContextException($"'{context.Protocol}' is not a valid protocol");
+            }
+        }
+
+        public IInputResponseParser GetInputParser(Context context, Input input)
+        {
+            switch (context.Protocol)
+            {
+                case IPX800Protocol.Http:
+                    return new IPX800v4GetInputHttpResponseParser();
+                
+                case IPX800Protocol.M2M:
+                    return new IPX800v4GetInputM2MResponseParser();
+                
+                default:
+                    throw new IPX800InvalidContextException($"'{context.Protocol}' is not a valid protocol");
+            }
+        }
+
+        public IGetOutputResponseParser GetOutputParser(Context context, Output input)
+        {
+            switch (context.Protocol)
+            {
+                case IPX800Protocol.Http:
+                    return new IPX800v4GetOutputHttpResponseParser();
+                
+                case IPX800Protocol.M2M:
+                    return new IPX800v4GetOutputM2MResponseParser();
+                
+                default:
+                    throw new IPX800InvalidContextException($"'{context.Protocol}' is not a valid protocol");
+            }
+        }
+
+        public ISetOutputResponseParser GetSetOutputParser(Context context, Output input)
+        {
+            switch (context.Protocol)
+            {
+                case IPX800Protocol.Http:
+                    return new IPX800v4SetOutputHttpResponseParser();
+                
+                case IPX800Protocol.M2M:
+                    return new IPX800v4SetOutputM2MResponseParser();
+                
+                default:
+                    throw new IPX800InvalidContextException($"'{context.Protocol}' is not a valid protocol");
+            }
+        }
+    }
+}
