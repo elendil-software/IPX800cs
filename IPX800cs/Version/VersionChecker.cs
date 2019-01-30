3b6e0440eb941f558b28e1781945a66fad8842a4
Julien Tschäppät <git@elendil.software>
2019-01-30 20:41:40 +0100

    Added VersionChecker

---
+++ IPX800cs/Version/VersionChecker.cs
@@ -0,0 +1,16 @@
+﻿namespace software.elendil.IPX800.Version
+{
+    public static class VersionChecker
+    {
+        public static bool IsLegacy(System.Version version)
+        {
+            if (version == null)
+            {
+                return false;
+            }
+
+            var version30542 = new System.Version("3.05.42");
+            return version.CompareTo(version30542) < 0;
+        }
+    }
+}
