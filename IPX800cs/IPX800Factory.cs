7bb47028f2767a1a9f9904b7914361a2db0277a8
Julien Tschäppät <git@elendil.software>
2019-02-01 21:08:41 +0100

    Factory refactoring

--- IPX800cs/IPX800Factory.cs
+++ IPX800cs/IPX800Factory.cs
@@ -4,29 +4,23 @@ using software.elendil.IPX800.Version;
 
 namespace software.elendil.IPX800
 {
-	public class IPX800Factory
+	public static class IPX800Factory
 	{
-		/// <summary>
-		/// Gets the IPX800 implementation by guess. 
-		/// First it try to get the IPX800 firmware version and the corresponding implementation. 
-		/// If it fails <see cref="TryToGuess"/> is called
-		/// </summary>
-		/// <param name="ip">The ip</param>
-		/// <param name="port">The port</param>
-		/// <param name="ipx800Version">The version of the IPX800</param>
-		/// <param name="pass">password</param>
-		/// <returns>An IIPX800 implementation.</returns>
-		/// <exception cref="IPX800UnknownVersionException">Thrown if unable to guess the version</exception>
+		public static IIPX800v2 GetIPX800v2Instance(string ip, int port, IPX800Protocol protocol, string user = null, string password = null)
+		{
+			Context context = new Context(ip, port, protocol, IPX800Version.V2, user, password);
+			return new IPX800v2(context);
+		}
 		
-		public static IIPX800 GetInstance(string ip, int port, IPX800Protocol protocol, IPX800Version ipx800Version, string user, string password)
-        {
-            Context context = new Context(ip, port, protocol, ipx800Version, user, password);
-
-            if (ipx800Version == IPX800Version.V3)
-            {
-	            context = GetIPX800v3Context(ip, port, protocol, ipx800Version, user, password);
-            }
-
+		public static IIPX800v4 GetIPX800v4Instance(string ip, int port, IPX800Protocol protocol, string user = null, string password = null)
+		{
+			Context context = new Context(ip, port, protocol, IPX800Version.V4, user, password);
+			return new IPX800v4(context);
+		}
+		
+		public static IIPX800v3 GetIPX800v3Instance(string ip, int port, IPX800Protocol protocol, string user = null, string password = null)
+        {          
+            Context context = GetIPX800v3Context(ip, port, protocol, IPX800Version.V3, user, password);
             return new IPX800v3(context);
 		}
 
