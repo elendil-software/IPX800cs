5fe94e8a3a69e4d89c8377f5c280de249d939da2
Julien Tschäppät <git@elendil.software>
2019-02-06 21:00:34 +0100

    corrected parameter name

--- IPX800cs/Commands/Builders/ICommandBuilderFactory.cs
+++ IPX800cs/Commands/Builders/ICommandBuilderFactory.cs
@@ -5,7 +5,7 @@ namespace IPX800cs.Commands.Builders
     public interface ICommandBuilderFactory
     {
         ISetOutputCommandBuilder GetSetOutputCommandBuilder(Context context, Output output);
-        IGetOutputCommandBuilder GetGetOutputCommandBuilder(Context context, Output input);
+        IGetOutputCommandBuilder GetGetOutputCommandBuilder(Context context, Output output);
         IGetInputCommandBuilder GetGetInputCommandBuilder(Context context, Input input);
         IGetVersionCommandBuilder GetGetVersionCommandBuilder(Context context);
     }
