c4c9a8f89d5115731687ac8a1f1c87eb6636a2cb
Julien Tschäppät <git@elendil.software>
2019-02-06 20:43:28 +0100

    Classes documentation

--- IPX800cs/Contracts/IIPX800.cs
+++ IPX800cs/Contracts/IIPX800.cs
@@ -1,4 +1,5 @@
-﻿using IPX800cs.IO;
+﻿using IPX800cs.Exceptions;
+using IPX800cs.IO;
 
 namespace IPX800cs.Contracts
 {
@@ -8,45 +9,52 @@ namespace IPX800cs.Contracts
 		/// Gets the state of an input
 		/// </summary>
 		/// <param name="inputNumber">The input number.</param>
-		/// <returns>The input state.</returns>
-		/// <exception cref="IPX800Exception">Thrown if it was unable to  get a response</exception>
-		/// <exception cref="IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
-		/// <exception cref="IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
+		/// <returns>The current input state.</returns>
+		/// <exception cref="IPX800SendCommandException">Thrown if it was unable to send the command or if an error occured while sending the command</exception>
+		/// <exception cref="IPX800InvalidResponseException">Thrown if the command has been successfully sent but the response can not be parsed</exception>
 		InputState GetInput(int inputNumber);
-		
-		/// <summary>
-		/// Gets the value of an analog input
-		/// </summary>
-		/// <param name="inputNumber">The input number.</param>
-		/// <returns>The value</returns>
-		/// <exception cref="IPX800Exception">Thrown if it was unable to  get a response</exception>
-		/// <exception cref="IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
-		/// <exception cref="IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
-		int GetAnalogInput(int inputNumber);
 
-		/// <summary>
-		/// Gets the state of an output
-		/// </summary>
-		/// <param name="outputNumber">The output number.</param>
-		/// <returns>The output state.</returns>
-		/// <exception cref="IPX800Exception">Thrown if it was unable to  get a response</exception>
-		/// <exception cref="IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
-		/// <exception cref="IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
-		OutputState GetOutput(int outputNumber);
+        /// <summary>
+        /// Gets the value of an analog input
+        /// </summary>
+        /// <param name="inputNumber">The input number.</param>
+        /// <returns>The current input value</returns>
+        /// <exception cref="IPX800SendCommandException">Thrown if it was unable to send the command or if an error occured while sending the command</exception>
+        /// <exception cref="IPX800InvalidResponseException">Thrown if the command has been successfully sent but the response can not be parsed</exception>
+        int GetAnalogInput(int inputNumber);
 
-		/// <summary>
-		/// Sets the state of an output.
-		/// </summary>
-		/// <param name="outputNumber">The output number.</param>
-		/// <param name="state">The state.</param>
-		/// <param name="fugitive">if set to <c>true</c>, the 'fugitive' mode will be used.</param>
-		/// <returns><c>true</c> if successful, <c>false</c> otherwise.</returns>
-		/// <exception cref="IPX800Exception">Thrown if it was unable to  get a response</exception>
-		/// <exception cref="IPX800ExecuteException">Thrown if it was unable to send the request or in case of timeout while waiting for response</exception>
-		/// <exception cref="IPX800ConnectionException">Thrown if the connexion with the IPX800 failed</exception>
-		bool SetOutput(int outputNumber, OutputState state);
-		
-		
-		bool SetDelayedOutput(int outputNumber);
+        /// <summary>
+        /// Gets the state of an output
+        /// </summary>
+        /// <param name="outputNumber">The output number.</param>
+        /// <returns>The current output state.</returns>
+        /// <exception cref="IPX800SendCommandException">Thrown if it was unable to send the command or if an error occured while sending the command</exception>
+        /// <exception cref="IPX800InvalidResponseException">Thrown if the command has been successfully sent but the response can not be parsed</exception>
+        OutputState GetOutput(int outputNumber);
+
+        /// <summary>
+        /// Sets the state of an output.
+        ///
+        /// Important, the method return <c>true</c> if the command has been successfully sent. But it doesn't mean that the IPX800 has correctly set the output.
+        /// Use the <see cref="GetOutput"/> method to check the state of the output.
+        /// </summary>
+        /// <param name="outputNumber">The output number.</param>
+        /// <param name="state">The state to set</param>
+        /// <returns><c>true</c> if successful, <c>false</c> otherwise.</returns>
+        /// <exception cref="IPX800SendCommandException">Thrown if it was unable to send the command or if an error occured while sending the command</exception>
+        /// <exception cref="IPX800InvalidResponseException">Thrown if the command has been successfully sent but the response can not be parsed</exception>
+        bool SetOutput(int outputNumber, OutputState state);
+
+        /// <summary>
+        /// Enables an output configured in delayed mode (also called "fugitive" mode)
+        ///
+        /// Important, the method return <c>true</c> if the command has been successfully sent. But it doesn't mean that the IPX800 has correctly set the output.
+        /// Use the <see cref="GetOutput"/> method to check the state of the output.
+        /// </summary>
+        /// <param name="outputNumber">The output number.</param>
+        /// <returns><c>true</c> if successful, <c>false</c> otherwise.</returns>
+        /// <exception cref="IPX800SendCommandException">Thrown if it was unable to send the command or if an error occured while sending the command</exception>
+        /// <exception cref="IPX800InvalidResponseException">Thrown if the command has been successfully sent but the response can not be parsed</exception>
+        bool SetDelayedOutput(int outputNumber);
 	}
 }
--- IPX800cs/Exceptions/IPX800SendCommandException.cs
+++ IPX800cs/Exceptions/IPX800SendCommandException.cs
@@ -2,9 +2,6 @@ using System;
 
 namespace IPX800cs.Exceptions
 {
-	/// <summary>
-	/// Exception that can be thrown when something wrong occurs while sending a command to an IPX800
-	/// </summary>
 	public class IPX800SendCommandException : IPX800Exception
 	{
         public IPX800SendCommandException() { }
