21fc468a515c1059daf40c3fab8104e1889aa9e7
Julien Tschäppät <git@elendil.software>
2019-02-06 20:43:28 +0100

    Classes documentation

--- IPX800cs/Contracts/IVersion.cs
+++ IPX800cs/Contracts/IVersion.cs
@@ -1,7 +1,11 @@
 namespace IPX800cs.Contracts
 {
-    public interface IVersionIO
+    public interface IVersion
     {
+        /// <summary>
+        /// Get the firmware version of the IPX800
+        /// </summary>
+        /// <returns>The firmware version</returns>
         System.Version GetVersion();
     }
 }
