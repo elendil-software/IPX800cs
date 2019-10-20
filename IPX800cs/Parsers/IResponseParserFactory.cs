9a71c1e14215b0adf65cca857efb77e937caa7b4
Julien Tschäppät <git@elendil.software>
2019-10-20 21:23:00 +0200

    Added parser factory for Get all I/O

--- IPX800cs/Parsers/IResponseParserFactory.cs
+++ IPX800cs/Parsers/IResponseParserFactory.cs
@@ -7,7 +7,9 @@ namespace IPX800cs.Parsers
         IGetVersionResponseParser GetVersionResponseParser(Context context);
         IAnalogInputResponseParser GetAnalogInputParser(Context context, Input input);
         IInputResponseParser GetInputParser(Context context, Input input);
+        IInputsResponseParser GetInputsParser(Context context, Input input);
         IGetOutputResponseParser GetOutputParser(Context context, Output output);
+        IGetOutputsResponseParser GetOutputsParser(Context context, Output output);
         ISetOutputResponseParser GetSetOutputParser(Context context, Output output);
     }
 }
