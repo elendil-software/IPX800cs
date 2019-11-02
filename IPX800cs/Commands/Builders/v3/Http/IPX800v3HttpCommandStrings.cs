78b17d8325d573db8a5eba7be0d9eb51e9e14147
Julien Tschäppät <git@elendil.software>
2019-11-02 20:29:04 +0100

    IPX800v3 HTTP : remplacement of xml by json request

--- IPX800cs/Commands/Builders/v3/Http/IPX800v3HttpCommandStrings.cs
+++ IPX800cs/Commands/Builders/v3/Http/IPX800v3HttpCommandStrings.cs
@@ -6,11 +6,10 @@ namespace IPX800cs.Commands.Builders.v3.Http
         public const string GetVersion = "globalstatus.xml";
         public const string SetOutput = "preset.htm?set";
         public const string SetOutputDelayed = "leds.cgi?led";
-        public const string GetOutput = "globalstatus.xml";
+        public const string GetOutput = "cmd=20";
         public const string GetOutputs = "cmd=20";
-        public const string GetDigitalInput = "globalstatus.xml";
-        public const string GetAnalogInput = "globalstatus.xml";
+        public const string GetDigitalInput = "cmd=10";
         public const string GetDigitalInputs = "cmd=10";
-        
+        public const string GetAnalogInput = "cmd=30";
     }
 }
