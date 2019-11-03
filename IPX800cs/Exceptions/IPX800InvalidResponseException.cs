0442d98441c823032c8c2a692df9bbe9cbc354e1
Julien Tschäppät <git@elendil.software>
2019-11-03 09:10:50 +0100

    Correction of ISerializable implementation in Exception classes

--- IPX800cs/Exceptions/IPX800InvalidResponseException.cs
+++ IPX800cs/Exceptions/IPX800InvalidResponseException.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace IPX800cs.Exceptions
 {
+    [Serializable]
     public class IPX800InvalidResponseException : IPX800Exception
     {
         public IPX800InvalidResponseException() { }
@@ -9,5 +11,7 @@ namespace IPX800cs.Exceptions
         public IPX800InvalidResponseException(string message) : base(message) { }
 
         public IPX800InvalidResponseException(string message, Exception innerException) : base(message, innerException) { }
+		
+        protected IPX800InvalidResponseException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
--- IPX800cs/Exceptions/IPX800SendCommandException.cs
+++ IPX800cs/Exceptions/IPX800SendCommandException.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace IPX800cs.Exceptions
 {
+	[Serializable]
 	public class IPX800SendCommandException : IPX800Exception
 	{
         public IPX800SendCommandException() { }
@@ -9,5 +11,7 @@ namespace IPX800cs.Exceptions
 		public IPX800SendCommandException(string message) : base(message) { }
 
 		public IPX800SendCommandException(string message, Exception innerException) : base(message, innerException) { }
+		
+		protected IPX800SendCommandException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 	}
 }
