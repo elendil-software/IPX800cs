23a28ce72aaec03765f1f1691081e6560d03a01b
Julien Tschäppät <git@elendil.software>
2019-11-03 09:10:50 +0100

    Correction of ISerializable implementation in Exception classes

--- IPX800cs/Exceptions/IPX800InvalidContextException.cs
+++ IPX800cs/Exceptions/IPX800InvalidContextException.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace IPX800cs.Exceptions
 {
+	[Serializable]
 	public class IPX800InvalidContextException : IPX800Exception
 	{
         public IPX800InvalidContextException() { }
@@ -9,5 +11,7 @@ namespace IPX800cs.Exceptions
 		public IPX800InvalidContextException(string message) : base(message) { }
 
 		public IPX800InvalidContextException(string message, Exception innerException) : base(message, innerException) { }
+		
+		protected IPX800InvalidContextException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 	}
 }
--- IPX800cs/Exceptions/IPX800NotSupportedCommandException.cs
+++ IPX800cs/Exceptions/IPX800NotSupportedCommandException.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace IPX800cs.Exceptions
 {
+    [Serializable]
     public class IPX800NotSupportedCommandException : IPX800Exception
     {
         public IPX800NotSupportedCommandException() { }
@@ -9,5 +11,7 @@ namespace IPX800cs.Exceptions
         public IPX800NotSupportedCommandException(string message) : base(message) { }
 
         public IPX800NotSupportedCommandException(string message, Exception innerException) : base(message, innerException) { }
+        
+        protected IPX800NotSupportedCommandException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
