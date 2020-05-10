9766f57cf09e440af2217be6a123f346d65810c7
Julien Tschäppät <git@elendil.software>
2020-05-10 22:13:36 +0200

    improvement of incorrect response handling for IPX800v2, M2M protocol
    If an unparsable response is received, the API will throw an
    IPX800InvalidResponseException containing the received response

--- IPX800cs/Parsers/v2/M2M/IPX800v2GetInputM2MResponseParser.cs
+++ IPX800cs/Parsers/v2/M2M/IPX800v2GetInputM2MResponseParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using IPX800cs.IO;
+using IPX800cs.Parsers.v3.Legacy.M2M;
 
 namespace IPX800cs.Parsers.v2.M2M
 {
@@ -7,6 +8,7 @@ namespace IPX800cs.Parsers.v2.M2M
     {
         public InputState ParseResponse(string ipxResponse, int inputNumber)
         {
+            ipxResponse.CheckResponse();
             var result = ipxResponse.Trim().Split('=')[1];
             return (InputState)Enum.Parse(typeof(InputState), result);
         }
