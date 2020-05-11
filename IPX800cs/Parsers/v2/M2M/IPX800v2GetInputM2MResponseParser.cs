80c1325a37274ca911350a34279639b4440e592a
Julien Tschäppät <git@elendil.software>
2020-05-11 12:51:03 +0200

    Refactoring of IPX800v2 M2M Parsers

--- IPX800cs/Parsers/v2/M2M/IPX800v2GetInputM2MResponseParser.cs
+++ IPX800cs/Parsers/v2/M2M/IPX800v2GetInputM2MResponseParser.cs
@@ -4,13 +4,13 @@ using IPX800cs.Parsers.v3.Legacy.M2M;
 
 namespace IPX800cs.Parsers.v2.M2M
 {
-    internal class IPX800v2GetInputM2MResponseParser : IInputResponseParser
+    internal class IPX800v2GetInputM2MResponseParser : IPX800v2M2MParserBase, IInputResponseParser
     {
         public InputState ParseResponse(string ipxResponse, int inputNumber)
         {
             ipxResponse.CheckResponse();
-            var result = ipxResponse.Trim().Split('=')[1];
-            return (InputState)Enum.Parse(typeof(InputState), result);
+            int value = GetValue(ipxResponse);
+            return (InputState) value;
         }
     }
 }
--- IPX800cs/Parsers/v2/M2M/IPX800v2GetOutputM2MResponseParser.cs
+++ IPX800cs/Parsers/v2/M2M/IPX800v2GetOutputM2MResponseParser.cs
@@ -3,13 +3,13 @@ using IPX800cs.Parsers.v3.Legacy.M2M;
 
 namespace IPX800cs.Parsers.v2.M2M
 {
-    internal class IPX800v2GetOutputM2MResponseParser : IGetOutputResponseParser
+    internal class IPX800v2GetOutputM2MResponseParser : IPX800v2M2MParserBase, IGetOutputResponseParser
     {
         public OutputState ParseResponse(string ipxResponse, int outputNumber)
         {
             ipxResponse.CheckResponse();
-            string result = ipxResponse.Trim().Split('=')[1];
-            return (OutputState)System.Enum.Parse(typeof(OutputState), result);
+            int value = GetValue(ipxResponse);
+            return (OutputState) value;
         }
     }
 }
