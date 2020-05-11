7ab2f099b2fe8d2270ff1b27c4fca56890165519
Julien Tschäppät <git@elendil.software>
2020-05-11 12:51:03 +0200

    Refactoring of IPX800v2 M2M Parsers

--- IPX800cs/Parsers/v2/M2M/IPX800v2GetAnalogInputM2MResponseParser.cs
+++ IPX800cs/Parsers/v2/M2M/IPX800v2GetAnalogInputM2MResponseParser.cs
@@ -2,13 +2,12 @@
 
 namespace IPX800cs.Parsers.v2.M2M
 {
-    internal class IPX800v2GetAnalogInputM2MResponseParser : IAnalogInputResponseParser
+    internal class IPX800v2GetAnalogInputM2MResponseParser : IPX800v2M2MParserBase, IAnalogInputResponseParser
     {
         public int ParseResponse(string ipxResponse, int inputNumber)
         {
             ipxResponse.CheckResponse();
-            var response = ipxResponse.Trim().Split('=');
-            return int.Parse(response[1]);
+            return GetValue(ipxResponse);
         }
     }
 }
---
+++ IPX800cs/Parsers/v2/M2M/IPX800v2M2MParserBase.cs
@@ -0,0 +1,11 @@
+namespace IPX800cs.Parsers.v2.M2M
+{
+    internal abstract class IPX800v2M2MParserBase
+    {
+        protected int GetValue(string ipxResponse)
+        {
+            string[] splitString = ipxResponse.Trim().Split('=');
+            return int.Parse(splitString[1]);
+        }
+    }
+}
