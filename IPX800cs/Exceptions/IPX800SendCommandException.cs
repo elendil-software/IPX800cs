da1f05ec8f5c73ae8b2255e1f5d5f92598c2c8cf
Julien Tschäppät <git@elendil.software>
2020-05-23 14:30:18 +0200

    refactoring of exceptions

--- IPX800cs/Exceptions/IPX800SendCommandException.cs
+++ IPX800cs/Exceptions/IPX800SendCommandException.cs
@@ -6,8 +6,6 @@ namespace IPX800cs.Exceptions
 	[Serializable]
 	public class IPX800SendCommandException : IPX800Exception
 	{
-        public IPX800SendCommandException() { }
-
 		public IPX800SendCommandException(string message) : base(message) { }
 
 		public IPX800SendCommandException(string message, Exception innerException) : base(message, innerException) { }
--- IPX800cs/Exceptions/IPX800UnknownVersionException.cs
+++ IPX800cs/Exceptions/IPX800UnknownVersionException.cs
@@ -6,8 +6,6 @@ namespace IPX800cs.Exceptions
 	[Serializable]
 	public class IPX800UnknownVersionException : IPX800Exception
 	{
-        public IPX800UnknownVersionException() { }
-
 		public IPX800UnknownVersionException(string message) : base(message) { }
 
 		public IPX800UnknownVersionException(string message, Exception innerException) : base(message, innerException) { }
