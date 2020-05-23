0bd9259a3dc89218cfea4229f8ebbdc79632e72f
Julien Tschäppät <git@elendil.software>
2020-05-23 14:20:03 +0200

    refactoring of IPX800InvalidResponseException constructors

--- IPX800cs/Exceptions/IPX800InvalidResponseException.cs
+++ IPX800cs/Exceptions/IPX800InvalidResponseException.cs
@@ -6,8 +6,6 @@ namespace IPX800cs.Exceptions
     [Serializable]
     public class IPX800InvalidResponseException : IPX800Exception
     {
-        public IPX800InvalidResponseException() { }
-
         public IPX800InvalidResponseException(string ipxResponse) : base($"'{ipxResponse}' is not a valid response") { }
 
         public IPX800InvalidResponseException(string ipxResponse, Exception innerException) : base($"'{ipxResponse}' is not a valid response", innerException) { }
