06ec7d03465c58b9088ec5d40adf37528b1ad675
Julien Tschäppät <git@elendil.software>
2021-12-13 18:04:23 +0100

    Remove GetVersion related implementations
    The only reason of GetVersion was to be able to know
    IPX800 v3 firmware version to support old firmwares

--- IPX800cs/Parsers/v2/M2M/IPX800v2GetAnalogInputM2MResponseParser.cs
+++ IPX800cs/Parsers/v2/M2M/IPX800v2GetAnalogInputM2MResponseParser.cs
@@ -1,6 +1,4 @@
-﻿using IPX800cs.Parsers.v3.Legacy.M2M;
-
-namespace IPX800cs.Parsers.v2.M2M
+﻿namespace IPX800cs.Parsers.v2.M2M
 {
     internal class IPX800v2GetAnalogInputM2MResponseParser : IPX800v2M2MParserBase, IAnalogInputResponseParser
     {
