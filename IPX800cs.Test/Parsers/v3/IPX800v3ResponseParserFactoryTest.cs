58f93e8129208f3e5af33d4453d2bfdde725b1d3
Julien Tschäppät <git@elendil.software>
2021-12-13 18:28:39 +0100

    Fix unit tests

--- IPX800cs.Test/Parsers/v3/IPX800v3ResponseParserFactoryTest.cs
+++ IPX800cs.Test/Parsers/v3/IPX800v3ResponseParserFactoryTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using IPX800cs.IO;
 using IPX800cs.Parsers.v3;
 using IPX800cs.Parsers.v3.Http;
-using IPX800cs.Parsers.v3.Legacy.Http;
 using IPX800cs.Parsers.v3.M2M;
 using IPX800cs.Version;
 using Xunit;
@@ -14,9 +13,8 @@ namespace IPX800cs.Test.Parsers.v3
     {
         public static IEnumerable<object[]> GetAnalogInputParserTestCases => new[]
         {
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Input { Type = InputType.AnalogInput }, typeof(IPX800v3GetAnalogInputHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Input { Type = InputType.AnalogInput }, typeof(IPX800v3GetAnalogInputM2MResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3, new System.Version(3,5,38)), new Input { Type = InputType.AnalogInput }, typeof(IPX800v3LegacyGetAnalogInputM2MResponseParser) },
+            new object[] { new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Input { Type = InputType.AnalogInput }, typeof(IPX800v3GetAnalogInputHttpResponseParser) },
+            new object[] { new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Input { Type = InputType.AnalogInput }, typeof(IPX800v3GetAnalogInputM2MResponseParser) }
         };
 
         [Theory]
@@ -37,7 +35,6 @@ namespace IPX800cs.Test.Parsers.v3
         {
             new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3GetInputHttpResponseParser) },
             new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3GetInputM2MResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3, new System.Version(3,5,38)), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3LegacyGetInputM2MResponseParser) }
         };
 
         [Theory]
@@ -57,9 +54,7 @@ namespace IPX800cs.Test.Parsers.v3
         public static IEnumerable<object[]> GetInputsParserTestCases => new[]
         {
             new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3GetInputsHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3, new System.Version(3,5,38)), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3LegacyGetInputsHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3GetInputsM2MResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3, new System.Version(3,5,38)), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3LegacyGetInputsM2MResponseParser) }
+            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Input { Type = InputType.DigitalInput }, typeof(IPX800v3GetInputsM2MResponseParser) }
         };
 
         [Theory]
@@ -79,8 +74,7 @@ namespace IPX800cs.Test.Parsers.v3
         public static IEnumerable<object[]> GetOutputParserTestCases => new[]
         {
             new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputM2MResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3, new System.Version(3,5,38)), new Output { Type = OutputType.Output }, typeof(IPX800v3LegacyGetOutputM2MResponseParser) }
+            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputM2MResponseParser) }
         };
 
         [Theory]
@@ -99,10 +93,8 @@ namespace IPX800cs.Test.Parsers.v3
 
         public static IEnumerable<object[]> GetOutputsParserTestCases => new[]
         {
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputsHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3, new System.Version(3,5,38)), new Output { Type = OutputType.Output }, typeof(IPX800v3LegacyGetOutputsHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputsM2MResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3, new System.Version(3,5,38)), new Output { Type = OutputType.Output }, typeof(IPX800v3LegacyGetOutputsM2MResponseParser) }
+            new object[] { new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputsHttpResponseParser) },
+            new object[] { new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3GetOutputsM2MResponseParser) }
         };
 
         [Theory]
@@ -123,8 +115,7 @@ namespace IPX800cs.Test.Parsers.v3
         public static IEnumerable<object[]> SetOutputParserTestCases => new[]
         {
             new object[] {new Context("192.168.1.2", 80, IPX800Protocol.Http, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3SetOutputHttpResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3SetOutputM2MResponseParser) },
-            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3, new System.Version(3,5,38)), new Output { Type = OutputType.Output }, typeof(IPX800v3LegacySetOutputM2MResponseParser) },
+            new object[] {new Context("192.168.1.2", 80, IPX800Protocol.M2M, IPX800Version.V3), new Output { Type = OutputType.Output }, typeof(IPX800v3SetOutputM2MResponseParser) }
         };
 
         [Theory]
