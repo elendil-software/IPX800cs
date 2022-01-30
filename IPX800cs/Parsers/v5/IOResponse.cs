7bdb3ae93859b75ef28345aa5780a89fdf717224
Julien Tschäppät <git@elendil.software>
2022-01-30 21:44:30 +0100

    IPX800 v5 IO response parser

---
+++ IPX800cs/Parsers/v5/IOResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace IPX800cs.Parsers.v5;
+
+internal class IOResponse
+{
+    [JsonProperty("_id")]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Link0 { get; set; }
+    public int Link1 { get; set; }
+    public bool Virtual { get; set; }
+    public bool On { get; set; }
+}
---
+++ IPX800cs/Parsers/v5/JsonResponseParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using IPX800cs.Exceptions;
+using Newtonsoft.Json;
+
+namespace IPX800cs.Parsers.v5;
+
+internal static class JsonResponseParser
+{
+    public static IEnumerable<IOResponse> ParseIO(this string jsonResponse)
+    {
+        return Parse<IOResponse>(jsonResponse);
+    }
+
+    private static IEnumerable<T> Parse<T>(string jsonResponse) where T : class
+    {
+        try
+        {
+            var parsedResponse = JsonConvert.DeserializeObject<List<T>>(jsonResponse);
+            if (parsedResponse == null || parsedResponse.Count == 0)
+            {
+                throw new IPX800InvalidResponseException($"JSON response is not valid : {jsonResponse}");
+            }
+
+            return parsedResponse;
+        }
+        catch (Exception e) when (e is not IPX800InvalidResponseException)
+        {
+            throw new IPX800InvalidResponseException(e.Message, e);
+        }
+    }
+}
