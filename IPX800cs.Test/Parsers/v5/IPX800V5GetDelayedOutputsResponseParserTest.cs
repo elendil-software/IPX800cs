2bfeb806a3e5ba16023c43e25b8b98446d5e5496
Julien Tschäppät <git@elendil.software>
2022-06-06 08:40:18 +0200

    Extract IPX800V5GetDelayedOutputsResponseParserTest in own file

---
+++ IPX800cs.Test/Parsers/v5/IPX800V5GetDelayedOutputsResponseParserTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IPX800cs.Exceptions;
+using IPX800cs.IO;
+using IPX800cs.Parsers.v5;
+using Xunit;
+
+namespace IPX800cs.Test.Parsers.v5;
+
+public class IPX800V5GetDelayedOutputsResponseParserTest
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(null)]
+    [InlineData("[]")]
+    [InlineData("{}")]
+    [InlineData("Some Invalid String")]
+    public void GivenInvalidResponse_ParseResponse_ThrowsInvalidResponseException(string invalidresponse)
+    {
+        //Arrange
+        var parser = new IPX800V5GetDelayedOutputsResponseParser();
+
+        //Act/Assert
+        Assert.Throws<IPX800InvalidResponseException>(() => parser.ParseResponse(invalidresponse));
+    }
+        
+    [Fact]
+    public void GivenValidResponse_ParseResponse_ReturnsExpectedValues()
+    {
+        //Arrange
+        var parser = new IPX800V5GetDelayedOutputsResponseParser();
+        
+        //Act
+        List<OutputResponse> response = parser.ParseResponse(IPX800V5Response.IOJson).ToList();
+            
+        //Assert
+        Assert.Equal(63, response.Count);
+    }
+}
